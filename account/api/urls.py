eab194921e77342aec765a7b039d830acdcb929c
LukaAlhonen <luka.alhonen@protonmail.com>
2024-01-25 11:39:59 +0200

    Rolled back to pre-production fiasco

--- account/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from account.api.views import RegisterUserAPI, UpdateUserAPI
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-  path('signup/', RegisterUserAPI.as_view()),
-  path('signin/', TokenObtainPairView.as_view()),
-  path('update/', UpdateUserAPI.as_view())
-]
